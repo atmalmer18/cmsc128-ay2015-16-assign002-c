fbc576eb766adf82211625b6b7059cdfd1db2a03
atmalmer18 <atmalmer23@gmail.com>
2016-03-06 23:41:52 +0800

    Project initialized

---
+++ library.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int getHammingDistance (char * str1, char * str2) {
+	return 0;
+}
+
+int countSubstrPattern (char * strToCheck, char * pattern) {
+	return 0;
+}
+
+int isValidString (char * strToCheck, char * alphabet) {
+	return 0;	
+}
+
+int getSkew (char * strInput, int scope) {
+	return 0;
+}
+
+int getMaxSkewN (char * strInput, int scope) {
+	return 0;
+}
+
+int getMinSkewN (char * strInput, int scope) {
+	return 0;
+}
