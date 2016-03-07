c0e9a398c9885d9861f748c6ced9468798113859
atmalmer18 <atmalmer23@gmail.com>
2016-03-07 11:57:37 +0800

    Finalized evaluation if string is valid

--- library.c
+++ library.c
@@ -26,7 +26,34 @@ int countSubstrPattern (char * strToCheck, char * pattern) {
 }
 
 int isValidString (char * strToCheck, char * alphabet) {
-	return 0;
+	int i = 0, j = 0, isValid = 0, isValidPerSymbol[strlen(alphabet)];
+	
+	if (strlen(strToCheck) < 0 || strlen(alphabet) == 0) {
+		return -1;
+	}
+	
+	if (strlen(strToCheck) == 0) {
+		return 1;
+	}
+	
+	for (; i < strlen(strToCheck); i += 1) {
+		isValidPerSymbol[i] = 0;
+		for (j = 0; j < strlen(alphabet); j += 1) {
+			if (strToCheck[i] == alphabet[i]) {
+				isValidPerSymbol[i] = 1;
+			}
+		}
+	}
+	
+	for (i = 0; i < strlen(alphabet); i += 1) {
+		isValid = 1;
+		if (isValidPerSymbol[i] == 0) {
+			isValid = 0;
+			break;
+		}
+	}
+	
+	return isValid;
 }
 
 int getSkew (char * strInput, int scope) {
