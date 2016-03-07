9e24e303607b0a6ed0e4e206df6648a642fe6a51
atmalmer18 <atmalmer23@gmail.com>
2016-03-07 11:47:02 +0800

    Finalized getting the hamming distance functionality

--- library.c
+++ library.c
@@ -2,7 +2,23 @@
 #include <string.h>
 
 int getHammingDistance (char * str1, char * str2) {
-	return 0;
+	int i = 0, inversions = 0;
+	
+	if (strlen(str1) != strlen(str2)) {
+		return -1;
+	}
+	
+	if (strlen(str1) == 0) {
+		return 0;
+	}
+	
+	for (; i < strlen(str1); i += 1) {
+		if (str1[i] == str2[i]) {
+			inversions += 1;
+		}
+	}
+	
+	return inversions;
 }
 
 int countSubstrPattern (char * strToCheck, char * pattern) {
@@ -10,7 +26,7 @@ int countSubstrPattern (char * strToCheck, char * pattern) {
 }
 
 int isValidString (char * strToCheck, char * alphabet) {
-	return 0;	
+	return 0;
 }
 
 int getSkew (char * strInput, int scope) {
